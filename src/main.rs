e327e14e0487389aa133e4229f4d7c50fc2e9589
Megatank58 <tanumegatank@gmail.com>
2024-06-19 15:17:18 +0000

    chore: init

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,42 @@
 mod ast;
-mod data;
-mod interpreter;
 mod lexer;
 mod parser;
-mod standardlibrary;
 mod token;
+mod errors;
 
-use rustyline::{error::ReadlineError, DefaultEditor};
-use std::{fs::read_to_string, process::exit, time::Instant};
+use tower_lsp::jsonrpc::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-use clap::Parser as ClapParser;
-use lexer::Lexer;
-
-use crate::{interpreter::Interpreter, parser::Parser};
-
-#[derive(ClapParser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// Output debug information
-    #[clap(short, long, value_parser)]
-    debug: bool,
-
-    /// Only run the lexer and parser
-    #[clap(long, value_parser)]
-    dry_run: bool,
-
-    /// Print the time elapsed while executing code
-    #[clap(short, long, value_parser)]
-    time: bool,
-
-    /// Print the characters used as globals
-    #[clap(short, long, value_parser)]
-    globals: bool,
-
-    /// The path of file which is to be executed
-    #[clap()]
-    input: Option<String>,
+#[derive(Debug)]
+struct Backend {
+    client: Client,
 }
 
-fn main() {
-    let args = Args::parse();
-    let main = Instant::now();
-
-    if args.input.is_none() {
-        if args.globals {
-            println!("calcagebra v{}\n", version());
-            let _ = Interpreter::new()
-                .init_globals()
-                .variables
-                .iter()
-                .map(|(a, b)| println!("{a} {b}"))
-                .collect::<Vec<_>>();
-            return;
-        }
-        println!(
-            "Welcome to calcagebra v{}\nTo exit, press CTRL+C or CTRL+D",
-            version()
-        );
-        let mut rl = DefaultEditor::new().unwrap();
-        let mut interpreter = Interpreter::new();
-        loop {
-            let readline = rl.readline("\x1b[1m\x1b[32m[In]:\x1b[0m ");
-            match readline {
-                Ok(line) => {
-                    if line.trim() == "exit" {
-                        break;
-                    }
-
-                    println!("\x1b[1m\x1b[31m[Out]:\x1b[0m ");
-
-                    interpreter.run(Parser::new(Lexer::new(&line).tokens()).ast());
-                }
-                Err(ReadlineError::Interrupted) => {
-                    println!("CTRL-C");
-                    break;
-                }
-                Err(ReadlineError::Eof) => {
-                    println!("CTRL-D");
-                    break;
-                }
-                Err(err) => {
-                    println!("Error: {:?}", err);
-                    break;
-                }
-            }
-        }
-        exit(1);
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
     }
 
-    let contents = read_to_string(args.input.unwrap()).unwrap();
-
-    let tokens = Lexer::new(&contents).tokens();
-
-    if args.debug {
-        let duration = main.elapsed();
-        println!("LEXER: {tokens:?}\n\nTIME: {duration:?}\n");
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
     }
 
-    let ast = Parser::new(tokens).ast();
-
-    if args.debug {
-        let duration = main.elapsed();
-        println!("AST: {ast:?}\n\nTIME: {duration:?}\n");
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
     }
+}
 
-    if args.dry_run {
-        return;
-    }
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
 
-    Interpreter::new().run(ast);
-
-    if args.debug || args.time {
-        let duration = main.elapsed();
-        println!("\nTIME: {duration:?}");
-    }
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
 
 pub fn version() -> String {
